86a5a53a07ecd4e6f64d1d34e4a952adfdbd1946
Madhvi Salaria <anub6217@gmail.com>
2025-11-06 19:03:17 +0530

    Create initial App component for CI/CD pipeline

---
+++ react-ci-cd-pipeline/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>⚙️ CI/CD Pipeline Practice with GitHub Actions</h1>
+      <p>Every push automatically tests and builds the React app!</p>
+    </div>
+  );
+}
+
+export default App;
